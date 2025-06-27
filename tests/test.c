936bfe13a5085ac1df495c8e3040487d7f3f29a1
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2025-06-27 16:23:55 +0100

    removed unused code and formatted some sections for clarity

--- tests/test.c
+++ tests/test.c
@@ -4,8 +4,9 @@
 #include <assert.h>
 #include "wave.h"
 
-// Simple test for the update function
+// test for the update function
 void test_update() {
+    
     int id, p;
     int master_num_nodes = 10;
     int num_dt = 10;
@@ -17,6 +18,7 @@ void test_update() {
     // Determine local nodes
     int local_num_nodes = master_num_nodes / p;
     if (id == p - 1) {
+        
         local_num_nodes += master_num_nodes % p;
     }
 
@@ -27,26 +29,22 @@ void test_update() {
     // Call update and check output
     double *result = update(id, p, master_num_nodes, local_num_nodes, num_dt, dt, u_local);
     assert(result != NULL);
-
-    /*
-    // Basic validation: should not contain NaNs
-    for (int i = 0; i < local_num_nodes; i++) {
-        assert(result[i] == result[i]);  // NaN fails this
-    }
-
-    //free(result);
-    */
+    
     printf("On Processor %d: test_update passed.\n", id);
 }
 
-// Simple test for the collect function
+// test for the collect function
 void test_collect() {
+    
     int id, p;
     int master_num_nodes = 10;
     int num_dt = 10;
     double dt = 0.001;
 
+    // process I.D.
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+    // processor number
     MPI_Comm_size(MPI_COMM_WORLD, &p);
 
     int local_num_nodes = master_num_nodes / p;
@@ -69,11 +67,12 @@ void test_collect() {
 }
 
 int main(int argc, char *argv[]) {
+    
     MPI_Init(&argc, &argv);
 
+    // call test functions
     test_update();
     test_collect();
 
     MPI_Finalize();
-    return 0;
 }
